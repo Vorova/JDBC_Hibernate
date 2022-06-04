ff3e551a3390a8a70b740b3ac64dee511e17516d
Vorova <vvvorova@gmail.com>
2022-06-04 09:48:56 +0500

    - UserDaoHibernateImpl - реализован! + сняты комментарии

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,24 +11,24 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         UserService userService = new UserServiceImpl();
-//
-//        userService.createUsersTable();
-//
-//        userService.saveUser("Andrey", "Vasilev", (byte) 25);
-//        userService.saveUser("Maksim", "Andreev", (byte) 31);
-//        userService.saveUser("Vadim", "Ivanov", (byte) 21);
-//        userService.saveUser("Anton", "Trofimov", (byte) 25);
-//
-//        System.out.println();
-//
-//        List<User> users = userService.getAllUsers();
-//        for (User user : users) {
-//            System.out.println(user);
-//        }
+
+        userService.createUsersTable();
+
+        userService.saveUser("Andrey", "Vasilev", (byte) 25);
+        userService.saveUser("Maksim", "Andreev", (byte) 31);
+        userService.saveUser("Vadim", "Ivanov", (byte) 21);
+        userService.saveUser("Anton", "Trofimov", (byte) 25);
+
+        System.out.println();
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
 
         userService.cleanUsersTable();
 
-//        userService.dropUsersTable();
+        userService.dropUsersTable();
     }
 
 }
